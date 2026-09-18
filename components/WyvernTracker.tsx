5c26b3f8d75a23b8ebbc37352f77afeb7bfb2ac2
Dandan Ramilo Cañete <60306285+dandan005@users.noreply.github.com>
2026-09-18 13:05:59 +0800

    Update WyvernTracker.tsx

--- components/WyvernTracker.tsx
+++ components/WyvernTracker.tsx
@@ -32,12 +32,12 @@ function BossPortrait({ element, boss }: { element: Element; boss: BossSlot }) {
         <span className="eyebrow">WYVERN BOSS</span>
         <span className="boss-slot__element">{element.toUpperCase()} SLOT</span>
       </div>
-      <div className="boss-portrait" style={{ aspectRatio: "4 / 3", overflow: "hidden" }}>
+      <div className="boss-portrait" style={{ overflow: "hidden" }}>
         {boss.image ? (
           <img
             src={boss.image}
             alt={boss.alt}
-            style={{ width: "100%", height: "100%", objectFit: "contain" }}
+            style={{ width: "100%", height: "auto", display: "block" }}
           />
         ) : (
           <div className="boss-portrait__empty"><span className="boss-portrait__glyph">?</span><span>PORTRAIT SLOT</span></div>
